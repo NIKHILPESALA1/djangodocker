d2fe980ec1e99fc41a24d791eb96cb1ed5290ead
Nikhil Pesala <nikhilpesala4@gmail.com>
2025-09-19 00:20:41 +0530

    Updated project with latest changes

--- devops/urls.py
+++ devops/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),   # 👈 Root URL now points to your home view
 ]
+
